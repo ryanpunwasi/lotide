8cdf178921c8b7504d8014ee716a23645ee4d1dc
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-05-31 18:42:42 -0400

    Fixed linting errors

--- eqArrays.js
+++ eqArrays.js
@@ -7,11 +7,11 @@ const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) return false;
 
   for (let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) return false;
+    if (arr1[i] !== arr2[i]) return false;
   }
 
   return true;
- };
+};
 
 // TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
