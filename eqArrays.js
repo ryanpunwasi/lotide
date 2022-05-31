1017e4004920c7a311f3e956c7ba319ced2f7676
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-05-31 18:40:32 -0400

    Completed eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = (actual, expected) => {
+  let msg = actual === expected ? `✅ Assertion Passed: ${actual} === ${expected}` : `❌ Assertion Failed: ${actual} !== ${expected}`;
+  console.log(msg);
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+ };
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1], [1, 2, 3]), false);
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays(['r', 2, 'd', '2'], ['r', 2, 'd', 2]), false);
