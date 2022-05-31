871eb55351821312cbc08fbd6f9ccf159c855cca
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-05-31 18:43:27 -0400

    Fixed linting errors

--- head.js
+++ head.js
@@ -5,7 +5,7 @@ const assertEqual = (actual, expected) => {
 
 const head = array => {
   return array[0];
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
