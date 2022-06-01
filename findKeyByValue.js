ac4d0fa74d8ad438693d3647a95aa2523be1fad2
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-01 17:31:30 -0400

    Initial commit

---
+++ findKeyByValue.js
@@ -0,0 +1,8 @@
+const assertEqual = (actual, expected) => {
+  let msg = actual === expected ? `✅ Assertion Passed: ${actual} === ${expected}` : `❌ Assertion Failed: ${actual} !== ${expected}`;
+  console.log(msg);
+};
+
+const findKeyByValue = (obj, value) => {
+  
+};
