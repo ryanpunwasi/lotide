22e2f00326cd666b24ec4f92e0051284dba02f3c
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-01 10:03:53 -0400

    Added basic scaffolding to middle function

---
+++ middle.js
@@ -0,0 +1,27 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: Arrays are equal.`);
+  } else {
+    console.log(`❌ Assertion Failed: Arrays are not equal.`);
+  }
+};
+
+const middle = (arr) => {
+  let middle = [];
+  if (arr.length <= 2) {
+    return middle;
+  }
+
+  return middle;
+}
