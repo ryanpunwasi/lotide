23393981a18330853825c5f1df808d21ab319edf
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-01 17:47:19 -0400

    Added additional assertion tests, fixed linting errors

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -13,11 +13,16 @@ const findKeyByValue = (obj, value) => {
   return undefined;
 };
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
+  drama:  "The Wire",
+  fantasy: "Game of Thrones",
+  anime: "Dorohedoro",
+  western: "Breaking Bad"
 };
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue({}, "The Wire"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Breaking Bad"), "western");
