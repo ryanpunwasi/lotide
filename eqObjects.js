9c0ae2959474b60157daf63fd6ff8adc651dc835
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-01 18:49:23 -0400

    Added logic to check for identical keys and identical key-value pairs

--- eqObjects.js
+++ eqObjects.js
@@ -4,11 +4,19 @@ const assertEqual = (actual, expected) => {
 };
 
 const eqObjects = (obj1, obj2) => {
-  objOneKeys = obj1.keys();
-  objTwoKeys = obj2.keys();
+  objOneKeys = Object.keys(obj1);
+  objTwoKeys = Object.keys(obj2);
 
-  if (objOneKeys.length !== objTwoKeys.length) {
+  if (objOneKeys.length !== objTwoKeys.length) { // Checks if obj1 and obj2 have the same number of keys
     return false;
+  }
+
+  for (let key of objOneKeys) {
+    if (!objTwoKeys.includes(key)) { // Checks if obj1 and obj2 have identical keys
+      return false;
+    } else if(obj1[key] !== obj2[key]) { // Confirms if obj1 and obj2 have identical key-value pairs
+      return false
+    }
   }
 
   return true;
@@ -16,3 +24,8 @@ const eqObjects = (obj1, obj2) => {
 
 assertEqual(eqObjects({}, {}), true);
 assertEqual(eqObjects({ name: 'Cloud' }, {}), false);
+assertEqual(eqObjects({ team: "Toronto Raptors"}, { team: "Toronto Raptors" }), true);
+assertEqual(eqObjects({ team: 'Toronto Raptors'}, { team: "Miami Heat" }), false);
+assertEqual(eqObjects({ team: 'Toronto Raptors', player: 'Fred Vanvleet'}, { team: "Miami Heat" }), false);
+assertEqual(eqObjects({ diffKey: 'Toronto Raptors'}, { team: "Toronto Raptors" }), false);
+assertEqual(eqObjects({ east: 'TOR', west: 'GSW'}, { east: 'GSW', west: 'TOR' }), false);
