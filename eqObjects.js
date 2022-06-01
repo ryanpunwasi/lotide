83709c385ecbf611c919d9c4c53fa92d241add38
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-01 19:02:28 -0400

    Commented out unecessary conditional, added assertion cases for array values

--- eqObjects.js
+++ eqObjects.js
@@ -12,15 +12,22 @@ const eqObjects = (obj1, obj2) => {
   }
 
   for (let key of objOneKeys) {
-    if (!objTwoKeys.includes(key)) { // Checks if obj1 and obj2 have identical keys
-      return false;
-    } else if (obj1[key] !== obj2[key]) { // Confirms if obj1 and obj2 have identical key-value pairs
+    //if (!objTwoKeys.includes(key)) { // Checks if obj1 and obj2 have identical keys
+     // return false;
+    if (obj1[key] !== obj2[key]) { // Confirms if obj1 and obj2 have identical key-value pairs
       return false;
     }
   }
 
   return true;
 };
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);
 
 assertEqual(eqObjects({}, {}), true);
 assertEqual(eqObjects({ name: 'Cloud' }, {}), false);
@@ -29,3 +36,10 @@ assertEqual(eqObjects({ team: 'Toronto Raptors'}, { team: "Miami Heat" }), false
 assertEqual(eqObjects({ team: 'Toronto Raptors', player: 'Fred Vanvleet'}, { team: "Miami Heat" }), false);
 assertEqual(eqObjects({ diffKey: 'Toronto Raptors'}, { team: "Toronto Raptors" }), false);
 assertEqual(eqObjects({ east: 'TOR', west: 'GSW'}, { east: 'GSW', west: 'TOR' }), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
