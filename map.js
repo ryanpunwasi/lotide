b3fac1f646eb9566799d40efb7c888385230df05
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-02 16:00:43 -0400

    Added logic to loop through array and apply callback

--- map.js
+++ map.js
@@ -1,5 +1,12 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
-const map = () => {
+const map = (arr, cb) => {
+  const results = []
+  for (let item of arr) {
+    results.push(cb(item));
+  }
+  return results;
+};
 
-} ;  
+const results1 = map(words, word => word[0]);
+console.log(results1);
