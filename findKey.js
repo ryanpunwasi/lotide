a2eae863bf1b958fe0ed0435a1ddd60b6e5d2453
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-02 17:55:08 -0400

    Initial commit

---
+++ findKey.js
@@ -0,0 +1,8 @@
+const assertEqual = (actual, expected) => {
+  let msg = actual === expected ? `✅ Assertion Passed: ${actual} === ${expected}` : `❌ Assertion Failed: ${actual} !== ${expected}`;
+  console.log(msg);
+};
+
+const findKey = (obj, cb) => {
+  return undefined;
+};
