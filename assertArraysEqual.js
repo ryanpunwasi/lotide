04fc61a0501a5f6371700d34055f1e2f85c9faac
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-02 23:24:55 -0400

    Added inspect function from util library, used inspect to modify console log statements

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -9,11 +9,11 @@ const eqArrays = (arr1, arr2) => {
 };
 
 const assertArraysEqual = (arr1, arr2) => {
-
+  const inspect = require('util').inspect;
   if (eqArrays(arr1, arr2)) {
-    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+    console.log(`✅ Assertion Passed: ${inspect(arr1)} === ${inspect(arr2)}`);
   } else {
-    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+    console.log(`❌ Assertion Failed: ${inspect(arr1)} !== [${inspect(arr2)}`);
   }
 };
 
