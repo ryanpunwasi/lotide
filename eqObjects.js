160253f633ca02eb4b41f63e4995b7a6a5c52380
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-08 01:33:01 -0400

    Recursive refactor

--- eqObjects.js
+++ eqObjects.js
@@ -1,17 +1,5 @@
-const assertEqual = (actual, expected) => {
-  let msg = actual === expected ? `✅ Assertion Passed: ${actual} === ${expected}` : `❌ Assertion Failed: ${actual} !== ${expected}`;
-  console.log(msg);
-};
-
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) return false;
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-
-  return true;
-};
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
 const eqObjects = (obj1, obj2) => {
   let objOneKeys = Object.keys(obj1);
@@ -26,6 +14,10 @@ const eqObjects = (obj1, obj2) => {
       if (!eqArrays(obj1[key], obj2[key])) { // In the case that obj1[key] and obj[2] are arrays, check to see if they are identical
         return false;
       }
+    } else if (!Array.isArray(obj1[key]) && !Array.isArray(obj2[key]) && typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+      if (!eqObjects(obj1[key], obj2[key])) {
+        return false;
+      }
     } else if (obj1[key] !== obj2[key]) { // Confirms if obj1 and obj2 have identical key-value pairs
       return false;
     }
@@ -38,29 +30,43 @@ module.exports = eqObjects;
 
 
 // TEST CASES
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// assertEqual(eqObjects(ab, ba), true);
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true);
 
-// const abc = { a: "1", b: "2", c: "3" };
-// assertEqual(eqObjects(ab, abc), false);
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);
 
-// assertEqual(eqObjects({}, {}), true);
-// assertEqual(eqObjects({ name: 'Cloud' }, {}), false);
-// assertEqual(eqObjects({ team: "Toronto Raptors"}, { team: "Toronto Raptors" }), true);
-// assertEqual(eqObjects({ team: 'Toronto Raptors'}, { team: "Miami Heat" }), false);
-// assertEqual(eqObjects({ team: 'Toronto Raptors', player: 'Fred Vanvleet'}, { team: "Miami Heat" }), false);
-// assertEqual(eqObjects({ diffKey: 'Toronto Raptors'}, { team: "Toronto Raptors" }), false);
-// assertEqual(eqObjects({ east: 'TOR', west: 'GSW'}, { east: 'GSW', west: 'TOR' }), false);
+assertEqual(eqObjects({}, {}), true);
+assertEqual(eqObjects({ name: 'Cloud' }, {}), false);
+assertEqual(eqObjects({ team: "Toronto Raptors"}, { team: "Toronto Raptors" }), true);
+assertEqual(eqObjects({ team: 'Toronto Raptors'}, { team: "Miami Heat" }), false);
+assertEqual(eqObjects({ team: 'Toronto Raptors', player: 'Fred Vanvleet'}, { team: "Miami Heat" }), false);
+assertEqual(eqObjects({ diffKey: 'Toronto Raptors'}, { team: "Toronto Raptors" }), false);
+assertEqual(eqObjects({ east: 'TOR', west: 'GSW'}, { east: 'GSW', west: 'TOR' }), false);
 
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// assertEqual(eqObjects(cd, dc), true);
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
 
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// const cd3 = {c: [1, 2], d: [1, 2]};
-// const cd4 = {c: [1, 2], d: [1, 2]};
-// const cd5 = {c: [1], d: [1, 2]};
-// assertEqual(eqObjects(cd3, cd4), true);
-// assertEqual(eqObjects(cd4, cd5), false);
-// assertEqual(eqObjects(cd, cd2), false);
+const cd2 = { c: "1", d: ["2", 3, 4] };
+const cd3 = {c: [1, 2], d: [1, 2]};
+const cd4 = {c: [1, 2], d: [1, 2]};
+const cd5 = {
+  c: { 
+    d: { e : { f: 4 } }
+  }
+};
+assertEqual(eqObjects(cd3, cd4), true);
+assertEqual(eqObjects(cd4, cd5), false);
+assertEqual(eqObjects(cd5, {
+  c: { 
+    d: { e : { f: 4 } }
+  }
+}), true);
+
+assertEqual(eqObjects(cd5, {
+  c: { 
+    d: { e : { f: 5 } }
+  }
+}), false);
