92999e0e1907599763a6a95103b68c63332e31be
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-07 23:58:24 -0400

    Recursive refactor and test cases for recursive refactor

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -4,3 +4,7 @@ assertArraysEqual([], []);
 assertArraysEqual([1, 2], [1, 2]);
 assertArraysEqual(['a', 4], ['a', '4']);
 assertArraysEqual([24], ['a', 24, 25]);
+assertArraysEqual([1, 2, 3], [1, 2, 3])
+assertArraysEqual([], []);
+assertArraysEqual([1], [1, 2, 3])
+assertArraysEqual([1, 2, [3, [4, [[[[6]]]]]], 4], [1, 2, [3, [4, [[[[6]]]]]], 4]);
